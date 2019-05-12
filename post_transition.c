02f888f9bb447101874b6675e069f69b6e7604ce
Nilton Gomes Martins Junior <nilton.junior51@hotmail.com>
2019-05-11 21:39:39 -0300

    Update

--- post_transition.c
+++ post_transition.c
@@ -32,15 +32,37 @@ typedef struct town town;
 
 
 void print_all_packages(town t) {
+    printf("%s:\n", t.name);
+    for (int i = 0; i < t.offices_count; ++i)
+    {
+        printf("\t%d:\n", i);
+        for (int j = 0; j < t.offices[i].packages_count; ++j)
+            printf("\t\t%s\n", t.offices[i].packages[j].id);
+    }
 }
 
 void send_all_acceptable_packages(town* source, int source_office_index, town* target, int target_office_index) {	
 }
 
+int get_count_packages_town(town *t) {
+    int count_packages = 0;
+    for (int i = 0; i < t->offices_count; ++i)
+        count_packages += (t->offices)[i].packages_count;
+    return count_packages;
+}
+
 town town_with_most_packages(town* towns, int towns_count) {
+    town* t = towns;
+    while (towns_count--)
+        if (get_count_packages_town(towns + towns_count) > get_count_packages_town(t))
+            t = towns + towns_count;
+    return *t;
 }
 
 town* find_town(town* towns, int towns_count, char* name) {
+    for (int i = 0; i < towns_count; ++i)
+        if ((towns + i)->name == name)
+            return towns + i;
 }
 
 int main()
